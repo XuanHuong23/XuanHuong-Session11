c1c2bcc9d5699ba0448ab5dc375d740599ff8aa8
XuanHuong23 <xuanhuong18@gmail.com>
2024-06-04 20:14:24 +0900

    Add files via upload

--- baitap3.js
+++ baitap3.js
@@ -1,10 +1,11 @@
 let array = [1, 1, 2, 3, 4, 4, 5, 5, 5, 6, 7, 8, 8]
 let a = Number(prompt("Nhap mot so"))
-let count
+let count = 0
 for (let i = 0; i < array.length; i++) {
-    if (a == i) {
-        count++
+    if (a == array[i]) {
 
+        count++
     }
-    console.log("so phan tu trung nhau", count)
+
 }
+console.log("so phan tu trung nhau", count)
